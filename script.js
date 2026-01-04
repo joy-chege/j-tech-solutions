71d9430a07777cb14236d4ac56189736e9e0fa2e
joy-chege <joychege895@gmail.com>
2026-01-04 13:52:10 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function sendMessage(event) {
+    event.preventDefault();
+    document.getElementById("status").innerText =
+        "✅ Thank you! Your message has been sent. We will get back to you shortly.";
+}
